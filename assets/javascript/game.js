d23cd89314dcb4078e254db011c936abe2953db1
Abi D <axa.abigail.i@gmail.com>
2019-06-18 18:26:16 -0500

    created reset function and moved majority of functional code into gameReset.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,59 +5,85 @@ $(document).ready(function () {
 var wins = 0;
 var loss = 0;
 var score = 0;
+var targetNo = 0;
+var crystalArray = [];
 
-// generate random number to strive for
-var targetNo = Math.floor(Math.random()*50 + 1)
+
+// make a function for resetting the game ?
+function gameReset () {
+    crystalArray = [];
+    $("#score-calc").empty();
+    score = 0;
+    targetNo = Math.floor(Math.random()*75 + 1);
+    $("#score-calc").text(targetNo);
+
+    for (var i = 0; i < 4; i++) {
+
+        // function to generate random numbers for the crystals
+        crystalArray.push(Math.floor(Math.random()*15 + 1));
+    
+    }
+
+    for ( var i = 0; i < crystalArray.length; i++) {
+        $("#crys"+i).attr("data-value", crystalArray[i]);
+    
+    }
+
+    $("#win-no").text(wins);
+    $("#loss-no").text(loss);
+    $("#total-tally").text(score);
+
+
+}
 
 // console log targetNo to keep track of it
 console.log(targetNo);
 
 // display the targetNo in HTML
-$("#score-calc").text(targetNo);
-
-//empty array for numbers assigned to each crystal
-var crystalArray = [];
-
-// create a loop to push 4 random numbers between 1 and 50 into the crystalArray
-for (i=0; i < 4; i++) {
-
-    // function to generate random numbers for the crystals
-    crystalArray.push(Math.floor(Math.random()*15 + 1));
-
-}
-
-// console log crystal array to assure the populating loop is functioning
-console.log(crystalArray);
-
-// assign random numbers to buttons
-
-for ( j = 0; j < crystalArray.length; j++) {
+//$("#score-calc").text(targetNo);
 
 
-}
-$("#crys1").attr("data-number", crystalArray[0]);
-$("#crys2")
+// function to assign random numbers to buttons
+//for ( j = 0; j < crystalArray.length; j++) {}
 
 // populate wins, losses, and total score in corresponding divs & spans
-$("#win-no").text(wins);
-$("#loss-no").text(loss);
-$("#total-tally").text(score);
+
 
 // make an on click event to return the value of the crystal clicked
 
+// function to convert click into text
+
 $("button").on("click", function () {
 
-    $("total-tally").text(val(this) + score);
+    // how to refer to clicked value?
+    score = score + parseInt($(this).attr("data-value"));
 
+    console.log(this);
+    console.log($(this).attr("data-value"));
+    console.log(score);
+
+    // add clicked value to score
+    $("#total-tally").text(score);
+
+    // tally wins if number is matched
+if (score === targetNo) {
+    wins++;
+    $("#win-no").text(wins);
+    gameReset();
+
+} else if (score > targetNo) {
+    loss++;
+    $("#loss-no").text(loss);
+    gameReset();
+} 
 
 });
 
-// tally total score based on values assigned to crystals clicked
-
-// tally wins if number is matched
+gameReset();
 
 
-// tally losses for when number is exceeded
+
+
 
 //closing document.ready syntax
 }); 
